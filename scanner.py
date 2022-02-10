41be214bafe2315104c0dc730df11ee8b159c823
noura <1701595@eng.asu.edu.eg>
2022-02-10 02:27:56 +0200

    termination of process containing virus

---
+++ scanner.py
@@ -0,0 +1,23 @@
+import psutil
+import hashlib 
+import os
+import signal
+import pandas as pd
+for pr in psutil.process_iter(['pid','name','username']):
+ if pr.username()!='root':
+  filename= pr.cmdline()[0]
+ #if pr.name()=='timer': 
+  try:
+   with open(filename,"rb")as f:
+    bytes=f.read()
+    readable=hashlib.sha256(bytes).hexdigest();
+   
+   dataset=pd.read_csv('database.csv')
+   for r in dataset:
+    if r ==readable:
+     pi=pr.pid 
+     os.kill(pi,signal.SIGTERM)
+     
+      
+  except Exception:
+    pass
