12abde185042e96baee8ef0edce87454568f87c0
Mariamalaa015 <mariamalaa015@gmail.com>
2022-02-15 00:42:47 +0200

    Editing scanner 2 and 1
    sudo scanner 2 and using spaces in scanner 1

--- scanner.py
+++ scanner.py
@@ -3,21 +3,24 @@ import hashlib
 import os
 import signal
 import pandas as pd
+
 for pr in psutil.process_iter(['pid','name','username']):
- if pr.username()!='root':
-  filename= pr.cmdline()[0]
- #if pr.name()=='timer': 
-  try:
-   with open(filename,"rb")as f:
-    bytes=f.read()
-    readable=hashlib.sha256(bytes).hexdigest();
+    
+    if pr.username()!='root':
+        filename= pr.cmdline()[0]
+ 
+        try:
+            with open(filename,"rb")as f:
+                bytes=f.read()
+                readable=hashlib.sha256(bytes).hexdigest();
    
-   dataset=pd.read_csv('database.csv')
-   for r in dataset:
-    if r ==readable:
-     pi=pr.pid 
-     os.kill(pi,signal.SIGTERM)
+            #checking the database for signatures 
+            dataset=pd.read_csv('database.csv')
+            
+            for r in dataset:
+                if r ==readable:
+                    pi=pr.pid 
+                    os.kill(pi,signal.SIGTERM)
      
-      
-  except Exception:
-    pass
+        except Exception:
+            pass
